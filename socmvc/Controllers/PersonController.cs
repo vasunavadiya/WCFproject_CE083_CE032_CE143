87a83e52b79051f36e5e7a4d7144f5a704857579
vasunavadiya <68776047+vasunavadiya@users.noreply.github.com>
2021-02-22 08:24:12 +0530

    Add files via upload

---
+++ socmvc/Controllers/PersonController.cs
@@ -0,0 +1,57 @@
+﻿using socmvc.ServiceReference1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace socmvc.Controllers
+{
+    public class PersonController : Controller
+    {
+        // GET: Person
+        public ActionResult getPersons()
+        {
+            Service1Client o = new Service1Client();
+            List<Person> li = o.GetPersons().ToList();
+            ViewBag.List = li;
+            return View();
+        }
+        public ActionResult insertPerson()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult insertPerson(Person obj)
+        {
+
+            Service1Client o = new Service1Client();
+            o.InsertPerson(obj);
+            return View();
+        }
+        public ActionResult updatePerson()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult updatePerson(Person obj)
+        {
+            Service1Client o = new Service1Client();
+                o.UpdatePerson(obj);
+            return View();
+        }
+        public ActionResult deletePerson()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult deletePerson(int id)
+        {
+            Service1Client o = new Service1Client();
+            o.DeletePerson(id);
+            return View();
+        }
+
+    }
+
+}
